c7f7f66f591de2724700098b1064a3a694b89a3c
Josefine Lantz <jo.lantz@icloud.com>
2021-11-06 20:03:49 +0100

    implement delete post

--- app/main.py
+++ app/main.py
@@ -61,7 +61,8 @@ def create_posts(post: Post):
 
 @app.get("/posts/{id}")	
 def get_post(id: int):
-	cursor.execute(""" SELECT * from posts WHERE id = %s """, (str(id)))
+	cursor.execute(""" SELECT * from posts 
+	WHERE id = %s """, (str(id)))
 	post = cursor.fetchone()
 	if not post: 
 		raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
@@ -69,10 +70,11 @@ def get_post(id: int):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-	index = find_index_post(id) 
-	if index == None:
+	cursor.execute(""" DELETE from posts WHERE id = %s RETURNING * """, (str(id)))
+	deleted_post = cursor.fetchone()
+	conn.commit()
+	if deleted_post == None:
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist ")
-	my_posts.pop(index) 
 	return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
