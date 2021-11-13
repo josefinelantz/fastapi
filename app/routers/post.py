29848ecd2bc80392ddcf7418f060157b36ff6b21
Josefine Lantz <jo.lantz@icloud.com>
2021-11-13 17:39:07 +0100

    protect routes

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,6 +12,9 @@ router = APIRouter(
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 	posts = db.query(models.Post).all()
+	
+	# If we only want to fetch logged in users' posts 
+	#posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
 	# cursor.execute("""select * from posts """)
 	# posts = cursor.fetchall()
 
@@ -47,14 +50,15 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
 	# deleted_post = cursor.fetchone()
 	# conn.commit()
 	post_query = db.query(models.Post).filter(models.Post.id == id)
+	
 	post = post_query.first()
 
-	if post.first() == None:
+	if post == None:
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist ")
 	
-	if post.owner_id != oauth2.get_current_user.id:
+	if post.owner_id != current_user.id:
 		raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
-	post.delete(synchronize_session = False)
+	post_query.delete(synchronize_session = False)
 	db.commit()
 	return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -69,9 +73,9 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 	if post == None:
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist ")
-	if post.owner_id != oauth2.get_current_user.id:
+	if post.owner_id != current_user.id:
 		raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
-		
+
 	post_query.update(updated_post.dict(), synchronize_session=False)
 	db.commit()
 	# return updated post
